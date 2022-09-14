b829cb2d83c69737926c4d1d31b1263e7104e94a
dmtrth25 <vodolazhchenko25@gmail.com>
2022-09-14 19:59:19 +0300

    completed

---
+++ currency-converter/README.md
@@ -0,0 +1,13 @@
+Currency Converter
+
+Opportunity to convert UAH, USD, EUR, GBP
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+#### `yarn install`
+#### `yarn start`
---
+++ currency-converter/src/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Block } from './components/Block';
+
+import './index.scss';
+
+function App() {
+  const [fromCurrency, setFromCurrency] = useState('UAH')
+  const [toCurrency, setToCurrency] = useState('USD')
+  const [fromPrice, setFromPrice] = useState(0)
+  const [toPrice, setToPrice] = useState(1)
+
+  // const [rates, setRates] = useState({})
+  const ratesRef = useRef({})
+
+  useEffect(() => {
+    fetch('https://cdn.cur.su/api/latest.json')
+      .then(res => res.json())
+      .then(json => {
+        ratesRef.current = json.rates
+        onChangeToPrice(1)
+      })
+      .catch((err) => {
+        console.warn(err)
+        alert('Failed to get data')
+      })
+  }, [])
+
+  const onChangeFromPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency]
+    const result = price * ratesRef.current[toCurrency]
+    setToPrice(result.toFixed(1))
+    setFromPrice(value)
+  }
+
+  const onChangeToPrice = (value) => {
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value
+    setFromPrice(result.toFixed(1))
+    setToPrice(value)
+  }
+
+  useEffect(() => {
+    onChangeFromPrice(fromPrice)
+  }, [fromCurrency])
+
+  useEffect(() => {
+    onChangeToPrice(toPrice)
+  }, [toCurrency])
+
+  return (
+    <div className="App">
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
+    </div>
+  );
+}
+
+export default App;
